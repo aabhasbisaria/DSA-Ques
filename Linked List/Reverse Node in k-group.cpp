c2b6e6e37b2607cef3f1334a72c3dabc02b34ce0
Aabhas Bisaria <75527013+aabhasbisaria@users.noreply.github.com>
2023-07-19 13:57:01 +0530

    Reverse Node in k-group

---
+++ Linked List/Reverse Node in k-group.cpp
@@ -0,0 +1,44 @@
+Node *getListAfterReverseOperation(Node *head, int n, int b[]){
+	// Write your code here.
+	Node*temp=head;
+
+	vector<int>a;
+
+	while(temp)
+
+	{
+
+	a.push_back(temp->data);
+
+	temp=temp->next;
+
+	}
+
+	int sum=0;
+
+	for(int i=0;i<n;i++)
+
+	{
+
+	reverse(a.begin()+sum,min(a.begin()+sum+b[i],a.end()));
+
+	sum+=b[i];
+
+	}
+
+	temp=head;
+
+	int i=0;
+
+	while(temp)
+
+	{
+
+	temp->data=a[i++];
+
+	temp=temp->next;
+
+	}
+
+	return head;
+}
